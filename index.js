a84c7b69fc3ecef87f27b470bfc11d90ea801a2c
Owais Khan <oaak78692@gmail.com>
2023-09-25 18:03:12 +0530

    added delete functionality, end of development

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 main()
     .then(()=>{
@@ -52,6 +54,29 @@ app.post("/chats",(req,res)=>{
     });
     res.redirect("/chats");
 });
+// edit route
+app.get("/chats/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+});
+// Update route
+app.put("/chats/:id",async (req,res)=>{
+    let {id} = req.params;
+    let {msg : newMsg} = req.body;
+    let updatedChat =await Chat.findByIdAndUpdate(
+        id,
+        {msg : newMsg},
+        {runValidators : true, new : true}
+    );
+    res.redirect("/chats");
+});
+// Destroy route
+app.delete("/chats/:id", async (req,res)=>{
+    let {id} = req.params;
+    await Chat.findByIdAndDelete(id);
+    res.redirect("/chats");
+})
 
 app.listen(8080,()=>{
     console.log("Server is listening on port 8080");
